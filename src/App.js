97c9b0f0bffa045a7f3324f3981bcccc03969b9d
Matheus <matheussantos.quintanilha@gmail.com>
2023-08-09 20:02:28 -0300

    Categories component done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TopNav from "./components/TopNav";
 import Delivery from "./components/Delivery";
 import TopPicks from "./components/TopPicks";
 import Meal from "./components/Meal";
+import Categories from "./components/Categories";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Delivery />
       <TopPicks />
       <Meal />
+      <Categories />
     </div>
   );
 }
